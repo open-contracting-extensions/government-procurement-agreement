303f5ca0a4148a478003d9ad60fcc57932f78ee7
James McKinney <james@slashpoundbang.com>
2018-04-20 01:26:12 -0400

    Add Article VII:2

---
+++ docs/ocds.md
@@ -0,0 +1,8 @@
+# OCDS for GPA: Annotated Release Schema
+
+This document annotates selected fields of the release schema of OCDS, with details on relevant GPA clauses and any implementation notes. It is intended for a technical audience.
+
+```eval_rst
+  .. warning::
+    This page will be completed after the Annotated Revised GPA is agreed.
+```
--- schema/apply-extensions.py
+++ schema/apply-extensions.py
@@ -12,4 +12,11 @@ sys.path.append(os.path.join(basedir, '..', 'docs'))
 from conf import extension_registry_git_ref  # noqa
 
 apply_extensions(basedir, extension_registry_git_ref, 'gpa', [
+    'ocds_recurrence_extension',
+    'ocds_options_extension',
+    'ocds_procurementMethodModalities_extension',
+    'ocds_coveredBy_extension',
+    # 'ocds_party_classifications_extension',
+    # 'ocds_tender_mainItemClassification_extension',
+    # 'ocds_tender_legalBasisDetails_extension',
 ])
