5175e37d0798631d6d12653f077b49336842e29a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-21 23:00:06 -0500

    Use Sphinx 2.2.1 and update recommonmark and CommonMark

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,6 @@ from pathlib import Path
 
 import standard_theme
 from ocds_babel.translate import translate
-from recommonmark.parser import CommonMarkParser
 from recommonmark.transform import AutoStructify
 
 # -- General configuration ------------------------------------------------
@@ -39,7 +38,7 @@ from recommonmark.transform import AutoStructify
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,10 +47,6 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.md'
-source_parsers = {
-    '.md': CommonMarkParser,
-}
 
 # The master toctree document.
 master_doc = 'index'
